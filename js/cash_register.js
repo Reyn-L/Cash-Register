58e507f550c0b5f040e5c6a899a01e12b5763ed4
Reyn-L <Reyn.Leong@gmail.com>
2017-06-17 13:38:57 -1000

    changes after jon's live demo

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,16 +1,85 @@
 /*jshint esversion: 6 */
-(function(){
+(function(window){
   let cal = window.calculator;
   //getting buttons to work and show
+const key1 = document.getElementById("one");
+const keyDecimal = document.getElementById("decimal");
+const operatorEqual = document.getElementById("equal");
+const operatorPlus = document.getElementById("plus");
 const display = document.getElementById("display");
 const NUMBER = document.querySelectorAll("input");
-    NUMBER.forEach(function(num){
-      num.addEventListener("click", function(r){
-        display.innerHTML += r.target.value;
-        console.log(r.target.value);
-      });
+
+
+let operator = null;
+let input = "";
+
+//gets buttons to display
+NUMBER.forEach(function(num){
+/*num.addEventListener("click", function(r){
+  display.innerHTML += r.target.value;
+  });*/
+//funtion to debugg
+function debugAll(){
+  console.log({
+    action:action,
+    input:input,
+    operator:operator
+});}
+function keyPressed(event){
+setInput += event.target.dataset.value;
+renderDisplay();
+}
+key1.addEventListener("click", keyPressed);
+
+function renderDisplay(){
+display.innertext = input;
+}
+//sets new input
+function setInput(newInputValue){
+input = newInputValue;
+}
+//clears the input
+function clearInput(){
+input = " ";
+}
+//working on decimal
+keyDecimal.addEventListener("click", function(event){
+  if(input.indexOf(" .") < 0){
+    keyPressed(event);
+}
+ });
+});
+//adds
+operatorPlus.addEventListener("click",function(event){
+  if(operator !== null){
+    executeOperation();
+  }
+      operator = "ADD";
+        cal.load(parseFloat(input));
+          clearInput();
+});
+//working on equal sign to operate operators
+operatorEqual.addEventListener("click", function(event){
+switch(operator){
+  case "ADD":
+    cal.add(parseFloat(input) );
+    break;
+  default:
+  //throw new Error("operation $[operator] is not supported yet");
+}
+setInput(cal.getTotal());
+renderDisplay();
+});
+
+
+
+
+
+
+}(window));
+
 //gets display to show
-function getDisplay(){
+/*function getDisplay(){
   return display.innerHTML;
 }
 //function updateDisplay(){
@@ -18,52 +87,19 @@ function getDisplay(){
 
 function clearAll(){
   document.getElementById("display").value = "0.00";
-}
-let value = getDisplay();
-cal.load(Number(value));
+}*/
+/*let value = getDisplay();
+cal.load(Number(value));*/
 
-    });
-
-
-
-
-
-
-//adds
-let operator;
-
-function getSum(){
-operator = "+";
-cal.load(Number(display.innerHTML));
-display.innerHTML = "";
-}
 //equals
-function equal(){
+/*function equal(){
   let op = operator;
   let n = Number(display.innerHTML);
-  if(op === "+"){
+  if(op === "&plus;"){
     let ad = cal.add(n);
     display.innerHTML = ad;
-  }
-}
-
-//switch case for operators
-/*function operators(op, hi){
-  switch(op){
-    case "+": cal.add(hi);
-      break;
-    case "-": cal.subtract(hi);
-      break;
-    case "x": cal.multiply(hi);
-      break;
-    case "÷": cal.divide(hi);
-  }
-}*/
-
-
-
-
-}());
+  }*/
+//}
 
 //1st get load function working to store the memory
 //2nd create a new function to start on add
@@ -74,101 +110,6 @@ function equal(){
 //7th convert strings into numbers via Number(value) or parseInt
 //8th Reference calculator.js
 
-/*const DOUBLE = document.getElementById("double");
-const ZERO = document.getElementById("zero");
-const ONE = document.getElementById("one");
-const TWO = document.getElementById("two");
-const THREE = document.getElementById("three");
-const FOUR = document.getElementById("four");
-const FIVE = document.getElementById("five");
-const SIX = document.getElementById("six");
-const SEVEN = document.getElementById("seven");
-const EIGHT = document.getElementById("eight");
-const NINE= document.getElementById("nine");
-const DECIMAL = document.getElementById("decimal");
-const ADD = document.getElementById("plus");
-const SUBTRACT = document.getElementById("subtract");
-const MULTIPLY = document.getElementById("multiply");
-const DIVIDE = document.getElementById("divide");
-const EQUAL = document.getElementById("equal");
-const CLEAR = document.getElementById("clear");
-const BALANCE = document.getElementById("balance");
-const WITHDRAW_CASH = document.getElementById("withdraw");
-const DEPOSIT_CASH = document.getElementById("deposit");
-
-DOUBLE.addEventListener("click",  function(){
-  DISPLAY.innerHTML += "00";
-});
-ZERO.addEventListener("click", function(){
-  DISPLAY.innerHTML += 0;
-});
-ONE.addEventListener("click", function(){
-  DISPLAY.innerHTML += 1;
-});
-TWO.addEventListener("click", function(){
-  DISPLAY.innerHTML += 2;
-});
-THREE.addEventListener("click", function(){
-  DISPLAY.innerHTML += 3;
-});
-FOUR.addEventListener("click", function(){
-  DISPLAY.innerHTML += 4;
-});
-FIVE.addEventListener("click", function(){
-  DISPLAY.innerHTML += 5;
-});
-SIX.addEventListener("click", function(){
-  DISPLAY.innerHTML += 6;
-});
-SEVEN.addEventListener("click", function(){
-  DISPLAY.innerHTML += 7;
-});
-EIGHT.addEventListener("click", function(){
-  DISPLAY.innerHTML += 8;
-});
-NINE.addEventListener("click", function(){
-  DISPLAY.innerHTML += 9;
-});
-DECIMAL.addEventListener("click", function(){
-  DISPLAY.innerHTML += ".";
-});
-DIVIDE.addEventListener("click", function(){
-  DISPLAY.innerHTML = "÷";
-});
-MULTIPLY.addEventListener("click", function(){
-  DISPLAY.innerHTML = "×";
-});
-SUBTRACT.addEventListener("click", function(){
-  DISPLAY.innerHTML = "-";
-});
-ADD.addEventListener("click", function(){
-  DISPLAY.innerHTML = "+";
-});
-EQUAL.addEventListener("click", function(){
-  display.innerHTML = "=";
-});
-CLEAR.addEventListener("click", function(){
-  display.innerHTML = "0.00";
-});*/
-
-
-
-
-/*let operations = function(r) {
-  switch(experssion){
-    case "+":
-      cal.add(r);
-      break;
-    case "-":
-      cal.subtract(r);
-      break;
-    case "÷":
-      cal.divide(r);
-      break;
-    case "×":
-      cal.multiply(r);
-      break;
-  }*/
 
 
 
