788e3de4e1963ab5d1a77f5a1a4ad665e53fb316
Reyn-L <Reyn.Leong@gmail.com>
2017-06-16 19:33:31 -1000

    Made shorter codes and fixed display

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,67 +1,130 @@
 /*jshint esversion: 6 */
+(function(){
+  let cal = window.calculator;
 const display = document.getElementById("display");
-const double = document.getElementById("double");
-const zero = document.getElementById("zero");
-const one = document.getElementById("one");
-const two = document.getElementById("two");
-const three = document.getElementById("three");
-const four = document.getElementById("four");
-const five = document.getElementById("five");
-const six = document.getElementById("six");
-const seven = document.getElementById("seven");
-const eight = document.getElementById("eight");
-const nine = document.getElementById("nine");
-const decimal = document.getElementById("decimal");
-const add = document.getElementById("plus");
-const subtract = document.getElementById("subtract");
-const multiply = document.getElementById("multiply");
-const divide = document.getElementById("divide");
-const equal = document.getElementById("equal");
-const clear = document.getElementById("clear");
-const balance = document.getElementById("balance");
-const withdrawCash = document.getElementById("withdraw");
-const depositCash = document.getElementById("deposit");
+const NUMBER = document.querySelectorAll("input");
+    NUMBER.forEach(function(num){
+      num.addEventListener("click", function(r){
+        display.innerHTML += r.target.value;
+        console.log(r.target.value);
+      });
 
-double.addEventListener("click",  function(){
-  display.innerHTML += "00";});
-zero.addEventListener("click", function(){
-  display.innerHTML += 0;});
-one.addEventListener("click", function(){
-  display.innerHTML += 1;});
-two.addEventListener("click", function(){
-  display.innerHTML += 2;});
-three.addEventListener("click", function(){
-  display.innerHTML += 3;});
-four.addEventListener("click", function(){
-  display.innerHTML += 4;});
-five.addEventListener("click", function(){
-  display.innerHTML += 5;});
-six.addEventListener("click", function(){
-  display.innerHTML += 6;});
-seven.addEventListener("click", function(){
-  display.innerHTML += 7;});
-eight.addEventListener("click", function(){
-  display.innerHTML += 8;});
-nine.addEventListener("click", function(){
-  display.innerHTML += 9;});
-decimal.addEventListener("click", function(){
-  display.innerHTML += ".";});
-divide.addEventListener("click", function(){
-  display.innerHTML += "÷";});
-multiply.addEventListener("click", function(){
-  display.innerHTML += "×";});
-subtract.addEventListener("click", function(){
-  display.innerHTML += "-";});
-add.addEventListener("click", function(){
-  display.innerHTML += "+";
-equal.addEventListener("click", function(){
-  display.innerHTML += "=";});
-/*clear.addEventListener("click", function(){
-  display.innerHTML = " ";});*/
-clear.addEventListener("click", function (){
-  display.innerHTML = calc.wipe;});
-const cal = window.Calculator;
-console.log(cal.add);
+function getDisplay(){
+  return display.innerHTML;
+}
+let value = getDisplay();
+cal.load(Number(value));
+    });
+function getSum(){
+  cal.add();
+  return cal.add();
+}
 
+
+
+
+}());
+
+
+/*const DOUBLE = document.getElementById("double");
+const ZERO = document.getElementById("zero");
+const ONE = document.getElementById("one");
+const TWO = document.getElementById("two");
+const THREE = document.getElementById("three");
+const FOUR = document.getElementById("four");
+const FIVE = document.getElementById("five");
+const SIX = document.getElementById("six");
+const SEVEN = document.getElementById("seven");
+const EIGHT = document.getElementById("eight");
+const NINE= document.getElementById("nine");
+const DECIMAL = document.getElementById("decimal");
+const ADD = document.getElementById("plus");
+const SUBTRACT = document.getElementById("subtract");
+const MULTIPLY = document.getElementById("multiply");
+const DIVIDE = document.getElementById("divide");
+const EQUAL = document.getElementById("equal");
+const CLEAR = document.getElementById("clear");
+const BALANCE = document.getElementById("balance");
+const WITHDRAW_CASH = document.getElementById("withdraw");
+const DEPOSIT_CASH = document.getElementById("deposit");
+
+DOUBLE.addEventListener("click",  function(){
+  DISPLAY.innerHTML += "00";
 });
+ZERO.addEventListener("click", function(){
+  DISPLAY.innerHTML += 0;
+});
+ONE.addEventListener("click", function(){
+  DISPLAY.innerHTML += 1;
+});
+TWO.addEventListener("click", function(){
+  DISPLAY.innerHTML += 2;
+});
+THREE.addEventListener("click", function(){
+  DISPLAY.innerHTML += 3;
+});
+FOUR.addEventListener("click", function(){
+  DISPLAY.innerHTML += 4;
+});
+FIVE.addEventListener("click", function(){
+  DISPLAY.innerHTML += 5;
+});
+SIX.addEventListener("click", function(){
+  DISPLAY.innerHTML += 6;
+});
+SEVEN.addEventListener("click", function(){
+  DISPLAY.innerHTML += 7;
+});
+EIGHT.addEventListener("click", function(){
+  DISPLAY.innerHTML += 8;
+});
+NINE.addEventListener("click", function(){
+  DISPLAY.innerHTML += 9;
+});
+DECIMAL.addEventListener("click", function(){
+  DISPLAY.innerHTML += ".";
+});
+DIVIDE.addEventListener("click", function(){
+  DISPLAY.innerHTML = "÷";
+});
+MULTIPLY.addEventListener("click", function(){
+  DISPLAY.innerHTML = "×";
+});
+SUBTRACT.addEventListener("click", function(){
+  DISPLAY.innerHTML = "-";
+});
+ADD.addEventListener("click", function(){
+  DISPLAY.innerHTML = "+";
+});
+EQUAL.addEventListener("click", function(){
+  display.innerHTML = "=";
+});
+CLEAR.addEventListener("click", function(){
+  display.innerHTML = "0.00";
+});*/
+
+
+
+
+/*let operations = function(r) {
+  switch(experssion){
+    case "+":
+      cal.add(r);
+      break;
+    case "-":
+      cal.subtract(r);
+      break;
+    case "÷":
+      cal.divide(r);
+      break;
+    case "×":
+      cal.multiply(r);
+      break;
+  }*/
+
+
+
+
+
+
 
